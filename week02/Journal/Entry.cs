651a2cdf9031830522cfd9f1b47b3abcf7596eb1
Divine <nwokedidivine78@gmail.com>
2025-05-16 23:03:26 +0100

    Modified the Entry.cs file

--- week02/Journal/Entry.cs
+++ week02/Journal/Entry.cs
@@ -5,17 +5,19 @@ public class Entry
     public string _prompt { get; set; }
     public string _date { get; set; }
     public string _entryText { get; set; }
+    public string _mood { get; set; }
 
-    public Entry(string prompt, string date, string entryText)
+    public Entry(string prompt, string date, string entryText, string mood)
     {
         _prompt = prompt;
         _date = date;
         _entryText = entryText;
+        _mood = mood;
     }
 
     public void Display()
     {
-        Console.WriteLine($"{_date} - {_prompt}");
+        Console.WriteLine($"{_date} - {_prompt} - Mood: {_mood}");
         Console.WriteLine(_entryText);
     }
 }
